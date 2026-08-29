30237fcedd605cc88dd2f35fc96d73223845a177
Pradeep Chaudhary <mr.pradeepchy61@gmail.com>
2026-08-29 18:52:52 +0530

    change background to a random color

---
+++ backgroundcolorchange/src/App.jsx
@@ -0,0 +1,21 @@
+
+import { useState } from "react";
+function App(){
+  const [color, setColor] = useState("white");
+
+  function changeColor(){
+    const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+
+    setColor(randomColor);
+  }
+
+  return(
+    <div style={{backgroundColor: color , height: 300}}>
+      <h1>Background Color: {color}</h1>
+      <button onClick={changeColor}>Change Color</button>
+
+
+    </div>
+  )
+}
+export default App;
